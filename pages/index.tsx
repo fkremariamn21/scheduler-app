77c3e630ba5528413921647e35ded7653ae1c6dc
Fkremariam Nigatu <fkremariamn21@gmail.com>
2025-08-28 09:12:16 +0000

    welcome added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,37 @@
-import SchedulerForm from '../components/SchedulerForm';
+// pages/index.tsx
+import { useRouter } from 'next/router';
 import Head from 'next/head';
+import styles from '../styles/WelcomePage.module.css';
+import Image from 'next/image';
 import React from 'react';
 
-const Home: React.FC = () => {
+const WelcomePage: React.FC = () => {
+  const router = useRouter();
+
+  const handleContinue = () => {
+    router.push('/scheduler');
+  };
+
   return (
     <div>
       <Head>
-        <title>COB Runner Scheduler</title>
-        <meta name="description" content="An automatic scheduler for the Close of Business runner." />
+        <title>Welcome to COB Scheduler</title>
+        <meta name="description" content="Welcome page for the COB Runner Scheduler." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <SchedulerForm />
-      </main>
+      <div className={styles.container}>
+        <h1 className={styles.title1}>Nib International Bank</h1>
+        <h1 className={styles.title2}>IS Application Department</h1>
+        <h1 className={styles.title3}>Monthly COB Scheduler</h1>
+        <div className={styles.logoContainer}>
+          <Image src="/logo.png" alt="Nib International Bank Logo" width={200} height={200} />
+        </div>
+        <button className={styles.button} onClick={handleContinue}>
+          Continue
+        </button>
+      </div>
     </div>
   );
 };
 
-export default Home;
+export default WelcomePage;
